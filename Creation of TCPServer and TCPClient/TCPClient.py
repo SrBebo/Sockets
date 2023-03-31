5b9b628cc609c26bf922e4e57f245293fb21f9b9
Fabricio Simbaña <70413460+SrBebo@users.noreply.github.com>
2023-03-31 09:47:53 -0500

    Update and rename TCpClient.py to TCPClient.py

--- Creation of TCPServer and TCPClient/TCPClient.py
+++ Creation of TCPServer and TCPClient/TCPClient.py
@@ -4,11 +4,11 @@ import socket #importamos el modulo socket
 
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creamos el socket
 
-host = socket.gethostname() #obtenemos el nombre de la maquina
+#host = socket.gethostname() #obtenemos el nombre de la maquina
 
 port = 444 #reservamos un puerto para la conexion
 
-clientsocket.connect((host, port)) #conectamos con el servidor
+clientsocket.connect(('192.132.1.2', port)) #conectamos con el servidor
 
 message = clientsocket.recv(1024) #recibimos el mensaje del servidor
 
